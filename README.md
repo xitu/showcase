1dd105aba4125c1fc72caf8dd48d98195c5f0a92
akira-cn <19511344@qq.com>
2022-02-16 18:54:50 +0800

    :tada:first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Showcase
+
+通过命令行快速加载 Vue、React 组件。
---
+++ bin/index.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+const args = require('../lib/args');
+
+// TBD
+// const ShowCase = require('../lib/showcase');
+// const showcase = new ShowCase(args);
+// showcase.show();
