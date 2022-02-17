bd1e5859f29ae0374a239b7509e42079ab7f05d7
akira-cn <19511344@qq.com>
2022-02-17 18:31:17 +0800

    update cli

--- bin/index.js
+++ bin/index.js
@@ -2,7 +2,16 @@
 
 const args = require('../lib/args');
 
-// TBD
-// const ShowCase = require('../lib/showcase');
-// const showcase = new ShowCase(args);
-// showcase.show();
+const Showcase = require('../lib/showcase');
+const opts = {};
+if(args.repl) {
+  opts.client = {repl: true};
+} else if(args.client) {
+  opts.client = {output: args.output};
+}
+const showcase = new Showcase(opts);
+showcase.showComponent({
+  path: args.input,
+  open: !args.client && !args.repl,
+  events: args.events,
+});
